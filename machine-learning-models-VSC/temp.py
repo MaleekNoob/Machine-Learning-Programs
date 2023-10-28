a4f810e84b6133689214b98d44e66fc361881873
MaleekNoob <i221526@nu.edu.pk>
2023-10-28 22:35:35 +0500

    understanding moving average function

---
+++ machine-learning-models-VSC/temp.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def moving_average_forecast(series, window_size):
+  mov = np.cumsum(series)
+  return mov
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(moving_average_forecast(arr, 2))
