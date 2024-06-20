1d7fb428227120cab27c2e7e34c939162d48b8c9
Maleek Hussain Ali <115790326+MaleekNoob@users.noreply.github.com>
2024-06-21 00:20:14 +0500

    Add files via upload

---
+++ practice-problems/lonely integer.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    for i in range(len(a)):
+        count = 0
+        for j in range (len(a)):
+            if a[j] == a[i]:
+                count += 1
+        
+        if count == 1:
+            return a[i]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
